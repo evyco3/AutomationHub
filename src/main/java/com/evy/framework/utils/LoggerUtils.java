c1d3674c69d0b287ce167a166e14d3d324835204
evyco3 <evyco3@gmail.com>
2024-07-21 11:15:12 +0300

    Add configuration, driver management, and loggin utility classes
    - Added BrowserTypeConverter, ConfigManager, and FrameworkConfig for configuration management.
    - Added Driver, DriverFactory, and DriverSupplier for WebDriver management.
    - Updated LoggerUtils for improved logging.
    - Added config.properties for application settings.
    - Added log4j2.xml for Log4j2 configuration.

--- src/main/java/com/evy/framework/utils/LoggerUtils.java
+++ src/main/java/com/evy/framework/utils/LoggerUtils.java
@@ -4,29 +4,28 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * A utility class for logging messages and exceptions using Log4j2.
+ * Utility class for logging messages using Log4j2.
  * <p>
- * This class provides static methods for logging informational and error messages.
- * It uses Log4j2 for logging and ensures that logging is consistent across different classes.
+ * Provides static methods to log informational and error messages consistently.
  * </p>
  */
 public final class LoggerUtils {
 
     /**
-     * The logger instance for this class.
+     * Logger instance for this class.
      */
     private static final Logger logger = LogManager.getLogger(LoggerUtils.class);
 
     /**
-     * Private constructor to prevent instantiation of this utility class.
+     * Private constructor to prevent instantiation.
      */
     private LoggerUtils() {}
 
     /**
-     * Retrieves a logger instance for the specified class.
+     * Gets a logger for the specified class.
      *
-     * @param cls the class for which to retrieve the logger
-     * @return a Logger instance for the specified class
+     * @param cls the class for which to get the logger
+     * @return a Logger for the specified class
      */
     private static Logger getLogger(Class<?> cls) {
         return LogManager.getLogger(cls);
@@ -36,18 +35,18 @@ public final class LoggerUtils {
      * Logs an informational message for the specified class.
      *
      * @param cls the class for which to log the message
-     * @param msg the informational message to log
+     * @param msg the message to log
      */
     public static void info(Class<?> cls, String msg) {
         getLogger(cls).info(msg);
     }
 
     /**
-     * Logs an error message and a throwable for the specified class.
+     * Logs an error message and exception for the specified class.
      *
      * @param cls the class for which to log the error
      * @param msg the error message to log
-     * @param t   the throwable to log
+     * @param t   the exception to log
      */
     public static void error(Class<?> cls, String msg, Throwable t) {
         getLogger(cls).error(msg, t);
